bdc875d831d29e1bb9ebe249f9a5a92088ec745a
chandhiraleka <36121166+chandhiraleka@users.noreply.github.com>
2018-02-10 17:35:32 +0530

    Add files via upload

---
+++ Beginner level/volume.py
@@ -0,0 +1,8 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/cp/Documents/volume.py ==================
+enter the value of radious8
+volume of spere:2149.64
+volume 2149.64
+>>> 
